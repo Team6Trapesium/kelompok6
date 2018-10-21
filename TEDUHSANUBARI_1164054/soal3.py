473d602df0be4921761a29d30e76f68f965c1b86
teduhsanubari <37590284+teduhsanubari@users.noreply.github.com>
2018-10-21 22:58:08 +0700

    comit soal 3
    soal no 3

---
+++ TEDUHSANUBARI_1164054/soal3.py
@@ -0,0 +1,16 @@
+import shapefile                    #Modul shapefile yang di ambil dari pyshp
+w=shapefile.Writer(shapeType=1)     #Membuat shpfile baru
+
+w.shapeType=2                         #Mengecek point, line atau polygon, jika () berarti tidak 
+w.shapeType=3                        #Mengecek point, line atau polygon, jika () berarti tidak
+w.shapeType=4                         #Mengecek point, line atau polygon, jika () berarti tidak
+    
+w.field("kolom1","C")               #Membuat baris 1 dengan tipe character file dbf.
+w.field("kolom2","C")               #Membuat baris 2 dengan tipe character file dbf.
+
+w.record("ngek","satu")             #Isi dari kolom 1 dan baris 1 = ngek, 2 = 1
+w.record("ngok","dua")              #Isi dari kolom 2 dan baris 1 = ngek, 2 = 2
+    
+w.point(1,1)                        #Isi file shp dengan point atau titik, dimana x=1 dan y=1 untuk row 1
+w.point(2,2)                        #Isi file shp dengan point atau titik, dimana x=2 dan y=2 untuk row 1
+w.save("soal3")                     #Melakukan save dengan nama soal3
